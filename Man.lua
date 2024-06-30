9dc76d054979ddba9eb5b01065ee8397f6f6b727
Even-dev <samuca1234.oficial@gmail.com>
2024-06-30 01:15:22 -0300

    Create Man.lua
    Arquivo principal

---
+++ Man.lua
@@ -0,0 +1,25 @@
+print("selecione o caractere principal do seu trava")
+local car = io.read()  
+
+local lop = 1
+
+print("?")
+
+function  main()
+  
+  while lop < 100  do
+
+    print(car)
+    print(car)
+    print(car)
+    lop = lop + 10
+  
+end
+end
+
+local set = io.read()
+
+if set == "GOL" then 
+  main()
+  
+end
